8054a9aabffa6975d1f0c47dd605258e4d608fa4
dkibira <40610259+dkibira@users.noreply.github.com>
2018-09-02 13:35:33 -0400

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,21 @@
+setwd("c:\\kibira\\R-Course")
+myData <- read.table("household_power_consumption.txt", sep=";", header=T)
+newdata <- subset(myData, Date == "1/2/2007" | Date =="2/2/2007")
+newdata$Date <- as.Date(newdata$Date, format="%d/%m/%Y")
+newdata$Global_active_power <- as.character(newdata$Global_active_power)
+newdata$Global_active_power <- as.numeric(newdata$Global_active_power)
+
+dts1 <- paste(newdata$Date, newdata$Time)
+dts2 <- as.POSIXct(dts1, format = "%Y-%m-%d %H:%M:%S")
+#plot 4
+
+par(mfcol=c(2,2))
+plot(dts2, newdata$Global_active_power, type="l", xlab="", ylab= "Global Active Power(killowatts)")
+plot(dts2, newdata$Sub_metering_1,type="l",xlab="", ylab= "Energy sub metering", col="black")
+lines(dts2, newdata$Sub_metering_2,col="red")
+lines(dts2, newdata$Sub_metering_3,col="blue")
+legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black", "red", "blue"), lty=1:2, cex=0.8)
+plot(dts2, newdata$Voltage,type="l",ylab="Voltage",xlab="datetime")
+plot(dts2, newdata$Global_reactive_power, type="l",ylab="Global_reactive_power",xlab="datetime")
+dev.copy(png, file = "plot4.png", width = 480, height = 480)
+dev.off()
